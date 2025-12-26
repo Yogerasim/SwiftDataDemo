bfac32b95364f09e29c3bc1eb5702c12a92aba9b
PhilipGerasim <yogerrasim@gmail.com>
2025-12-26 18:44:59 +0300

    [Init] SwiftData demo project

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Филипп Герасимов on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
